423d66862794841cba58dec642a060c6aa9fbd64
Pillaulo <paulocevite@gmail.com>
2023-11-14 21:26:42 -0300

    solo indentacion

--- resources/views/front/account/register.blade.php
+++ resources/views/front/account/register.blade.php
@@ -2,50 +2,50 @@
 
 @section('content')
 <section class="section-5 pt-3 pb-3 mb-3 bg-white">
-        <div class="container">
-            <div class="light-font">
-                <ol class="breadcrumb primary-color mb-0">
-                    <li class="breadcrumb-item"><a class="white-text" href="#">Home</a></li>
-                    <li class="breadcrumb-item">Register</li>
-                </ol>
-            </div>
+    <div class="container">
+        <div class="light-font">
+            <ol class="breadcrumb primary-color mb-0">
+                <li class="breadcrumb-item"><a class="white-text" href="#">Home</a></li>
+                <li class="breadcrumb-item">Register</li>
+            </ol>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class=" section-10">
-        <div class="container">
-            <div class="login-form">    
-                <form action="" method="post" name="registrationForm" id="registrationForm">
-                    <h4 class="modal-title">Register Now</h4>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Name" id="name" name="name">
-                        <p></p>
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Email" id="email" name="email">
-                        <p></p>
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Phone" id="phone" name="phone">
-                        <p></p>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password" id="password" name="password">
-                        <p></p>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Confirm Password" id="password_confirmation" name="password_confirmation">
-                        <p></p>
-                    </div>
-                    <div class="form-group small">
-                        <a href="#" class="forgot-link">Forgot Password?</a>
-                    </div> 
-                    <button type="submit" class="btn btn-dark btn-block btn-lg" value="Register">Register</button>
-                </form>			
-                <div class="text-center small">Already have an account? <a href="login.php">Login Now</a></div>
-            </div>
+<section class=" section-10">
+    <div class="container">
+        <div class="login-form">
+            <form action="" method="post" name="registrationForm" id="registrationForm">
+                <h4 class="modal-title">Register Now</h4>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Name" id="name" name="name">
+                    <p></p>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Email" id="email" name="email">
+                    <p></p>
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Phone" id="phone" name="phone">
+                    <p></p>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Password" id="password" name="password">
+                    <p></p>
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Confirm Password" id="password_confirmation" name="password_confirmation">
+                    <p></p>
+                </div>
+                <div class="form-group small">
+                    <a href="#" class="forgot-link">Forgot Password?</a>
+                </div>
+                <button type="submit" class="btn btn-dark btn-block btn-lg" value="Register">Register</button>
+            </form>
+            <div class="text-center small">Already have an account? <a href="login.php">Login Now</a></div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
 
 @section('customJs')
@@ -58,7 +58,7 @@
         data: $(this).serializeArray(),
         dataType: 'json',
         success: function(response){
-            
+
             var errors = response.errors;
 
             if (response.status == false){
